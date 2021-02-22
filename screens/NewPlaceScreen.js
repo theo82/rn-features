5c507dd12bc0be52cd5b72da63f3da55a74ec309
theo82 <theoaristi53@gmail.com>
2021-02-22 08:23:53 +0200

    Getting Started with the Form

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const NewDetaiScreen = props => {
+    const [titleValue, setTitleValue] = useState('');
+
+    const titleChangeHandler = text => {
+       setTitleValue(text)
+    }
+
     return (
-        <View>
-            <Text>NewDetaiScreen</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput style={styles.textInput} value={titleValue}/>
+                <Button 
+                    title="Save Place" 
+                    color={Colors.primary} 
+                    onPress={() => {}}
+                />
+            </View>
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({ });
+const styles = StyleSheet.create({
+    form: {
+        margin: 30
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
+ });
 
 export default NewDetaiScreen;
